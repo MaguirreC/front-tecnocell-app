62100215f703e24a82edfcc78915703bc23f2f87
MaguirreC <maicolaguirre16@gmail.com>
2024-10-01 17:27:36 -0500

    configuracion lista

---
+++ src/router/index.js
@@ -0,0 +1,19 @@
+import {createRouter,createWebHistory} from 'vue-router'
+
+import Ejemplo from '../views/ejemplo.vue'
+
+
+const routes=[
+    {
+        path:'/ejemplo',
+        name:'Ejemplo',
+        component:Ejemplo
+    }
+]
+
+const router = createRouter({
+    history:createWebHistory(),
+    routes,
+})
+
+export default router;
---
+++ src/views/ejemplo.vue
@@ -0,0 +1,15 @@
+<script>
+
+export default{
+    name:'Ejemplo',
+}
+</script>
+
+<template>
+    <h1>Ejemplo</h1>
+
+</template>
+
+
+<style>
+</style>
